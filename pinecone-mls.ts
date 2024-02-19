2da1749cf7d452a1a3d1988c433e97b06bea5e50
Patrick Collins <patrickncollins@gmail.com>
2024-02-19 12:20:58 -0500

    Some cleanup

--- pinecone-mls.ts
+++ pinecone-mls.ts
@@ -1,47 +1,28 @@
 import {pinecone, openai} from "./app"
 import axios from "axios";
 import * as fs from "fs";
+import {RecordMetadata} from "@pinecone-database/pinecone";
 
-const index = pinecone.index("mls-pin");
+const index = pinecone.index("mls");
 
 function getListings(): object[]{
     // axios.get("https://api.bridgedataoutput.com/api/v2/OData/mlspin/Property?access_token=7016ecb8678df5cbde06715314d71bc7")
     const homes = JSON.parse( fs.readFileSync("./tmp/mls-values.json", {
         encoding: "utf-8"
     }));
-    return homes.slice(800, 1000).map((listing: any) => {
+    return homes.slice(900, 1000).map((listing: any) => {
         return {
             ListingKey: String(listing.ListingKey),
-            ShowingInstructions: String(listing.ShowingInstructions),
-            StreetName: String(listing.StreetName),
-            BathroomsFull: String(listing.BathroomsFull),
-            BedroomsTotal: String(listing.BedroomsTotal),
-            LotSizeAcres: String(listing.LotSizeAcres),
-            PostalCode: String(listing.PostalCode),
             PropertyType: String(listing.PropertyType),
-            ListPrice: String(listing.ListPrice),
-            StandardStatus: String(listing.StandardStatus),
-            StreetNumber: String(listing.StreetNumber),
-            ListingId: String(listing.ListingId),
-            YearBuilt: String(listing.YearBuilt),
-            StateOrProvince: String(listing.StateOrProvince),
-            ArchitecturalStyle: String(listing.ArchitecturalStyle),
-            City: String(listing.City),
-            BathroomsHalf: String(listing.BathroomsHalf),
             PropertySubType: String(listing.PropertySubType),
-            BuildingAreaTotal: String(listing.BuildingAreaTotal),
             PrivateOfficeRemarks: String(listing.PrivateOfficeRemarks),
             PublicRemarks: String(listing.PublicRemarks)
         }
     })
 }
 
-function createEmbedding(listing: object) {
-    let embedding = '';
-    for (const [key, value] of Object.entries(listing)) {
-        embedding += `${key}: ${value}\n`;
-    }
-    return embedding;
+function createEmbedding(listing: any) {
+    return `${listing.PropertySubType} - ${listing.PropertyType} - ${listing.PublicRemarks} - ${listing.PrivateOfficeRemarks}`;
 }
 
 async function uploadListings(listings: object[]) {
@@ -74,7 +55,6 @@ async function queryListings(search: string) {
     const query: any = await index.namespace('sample').query({
             topK: 2,
             vector: embedding.data[0].embedding,
-            includeValues: true,
             includeMetadata: true
         }
     );
@@ -84,7 +64,6 @@ async function queryListings(search: string) {
 }
 
 // uploadListings(getListings())
-
 
 queryListings("Small house with 1 bedroom and a porch").then(data => console.log(data.matches.map((m:any) => m.metadata)))
 // console.log(createEmbedding(getListings()[0]))
